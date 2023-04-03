e48063b4432783a30b3206b422aa47f5e7601817
demogit220 <gauravkumar_20329@aitpune.edu.in>
2023-04-03 17:20:08 +0530

    more test added

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,27 @@
+from sqlalchemy.exc import IntegrityError
+
 def test_user_model(user, database):
-    test_username = "student3"
-    test_email = "student3@fylebe.com"
+    test_username = "student4"
+    test_email = "student4@fylebe.com"
 
     test_user = user(username=test_username, email=test_email)
     database.session.add(test_user)
     database.session.commit()
 
-    assert test_user == user.get_by_email(test_email)
+    expected = "<User 'student4'>"
 
+    assert test_user == user.get_by_email(test_email)
+    assert repr(test_user) == expected
+
+def test_author_no_email(user,database):
+    test_username = "student4"
+    test_email = "student4@fylebe.com"
+
+    test_user = user(username=test_username, email=test_email)
+    
+    
+    database.session.add(test_user)
+    try:
+        database.session.commit()
+    except IntegrityError:
+        database.session.rollback()
